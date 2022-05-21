c14e96b2dc43e536016bac793419192fb508fbfa
Michał Kubita <toja@example.com>
2022-05-21 10:18:52 +0200

    remove warning call

--- samples/gland/gland_data.py
+++ samples/gland/gland_data.py
@@ -174,7 +174,7 @@ class GlandDataset(utils.Dataset):
 
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
-        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+        return mask.astype(bool), np.ones([mask.shape[-1]], dtype=np.int32)
 
     def image_reference(self, image_id):
         """Return the path of the image."""
